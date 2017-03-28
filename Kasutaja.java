b06bc3b9d443cf84f7628ae48790a724e25177b9
KristinaMeister <k.meister.kiku@gmail.com>
2017-03-28 23:20:26 +0300

    Add files via upload

---
+++ Kasutaja.java
@@ -0,0 +1,3 @@
+public class Kasutaja {
+    //vinge
+}
---
+++ Sündmus.java
@@ -0,0 +1,14 @@
+
+public class Sündmus {
+    private int kuupäev;
+    private int kuu;
+    private int aasta;
+    private String kirjeldus;
+
+    public Sündmus(int kuupäev, int kuu, int aasta, String kirjeldus) {
+        this.kuupäev = kuupäev;
+        this.kuu = kuu;
+        this.aasta = aasta;
+        this.kirjeldus = kirjeldus;
+    }
+}
